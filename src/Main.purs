68365e53658777347616d18e8fcfcbdcbab4e3a9
jacques <herbrand+git@protonmail.com>
2017-12-21 23:27:37 -0300

    Libraries added

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,19 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+
+data Lambda = Var String | App Lambda Lambda | Abs String Lambda
+
+instance showLambda :: Show Lambda where
+    show (Var x)   = x
+    show (Abs x t) = "\\" <> x <> "->" <> show t
+    show (App s t) = show s
+
+
+idL :: Lambda
+idL = Abs "x" $ Var "x"
+
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
